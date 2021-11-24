24edbeac64895dd40152f4e364529bffb2017a9e
rafaolivato <88344941+rafaolivato@users.noreply.github.com>
2021-11-24 10:12:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace treinando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float salario, soma=0,media;
+            int cont=0;
+            while(cont<3)
+            {
+              Console.WriteLine("Informe o valor do salário:");
+              salario = float.Parse(Console.ReadLine());
+              soma = soma + salario; //ACUMULADOR
+              cont = cont +1;        //CONTADOR
+            }
+            media = soma / cont;
+            Console.WriteLine("O valor médio dos salários é: {0:N}",media); 
+        }
+    }
+}
