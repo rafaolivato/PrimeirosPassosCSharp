b0bc90a66c37e7b58da27658c2423c663806a3f8
rafaolivato <88344941+rafaolivato@users.noreply.github.com>
2022-02-03 10:46:37 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace atv01_rafael
+namespace AT02_RafaelOlivato_UC04
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Pedido solicitar = new Pedido();
-
-            char continua = 'S';
-            do{
-
-              ItemPedido produto = new ItemPedido();
-              
-
-              Console.WriteLine("Informe o nome do item: ");
-
-              produto.descricaoPublico = Console.ReadLine();
-
-              Console.WriteLine("Informe o valor: ");
-
-              produto.valor_unitarioPublico = double.Parse(Console.ReadLine());
-
-
-              Console.WriteLine("Informe a quantidade: ");
-
-              produto.quantidadePublico = int.Parse(Console.ReadLine()); 
-
-              Console.WriteLine("A soma total do pedido é " +String.Format("{0:C2}", produto.registrar()));
-
-              solicitar.Adicionar (produto);
-
-              Console.WriteLine("Deseja continuar? (S) SIM (N) NÃO");  
-
-              continua = char.Parse(Console.ReadLine()); 
-
-              Console.Clear();
-
-            } while(continua=='S');
-
-            solicitar.Listar();
-
-            Console.WriteLine( "Total " + String.Format("{0:C2}",solicitar.FechamentoConta()));
-       
-
-            
-
-
-
-           
-            
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
